4f84da723ef24255b9151574b5f541b30d1cf6cc
Memoryman2022 <rlacannon@gmail.com>
2024-08-04 16:36:14 +0200

    pred route

--- routes/prediction.router.js
+++ routes/prediction.router.js
@@ -50,4 +50,17 @@ router.get("/:userId", authenticateToken, async (req, res) => {
   }
 });
 
+// New route to get predictions by fixtureId
+router.get("/fixture/:fixtureId", authenticateToken, async (req, res) => {
+  try {
+    const predictions = await Prediction.find({
+      fixtureId: req.params.fixtureId,
+    });
+    res.status(200).json(predictions);
+  } catch (error) {
+    console.error("Error fetching predictions:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 module.exports = router;
